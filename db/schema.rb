3c8ec6f3a487d45e9c7f294649d6969e7323cea1
ViktorPresniatski <presniatski@gmail.com>
2017-09-15 15:35:30 +0300

    add join_table businesses_company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20170914130126) do
+ActiveRecord::Schema.define(:version => 20170915122344) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -82,6 +82,11 @@ ActiveRecord::Schema.define(:version => 20170914130126) do
 
   add_index "businesses_categories", ["business_id"], :name => "index_businesses_categories_on_business_id"
   add_index "businesses_categories", ["category_id"], :name => "index_businesses_categories_on_category_id"
+
+  create_table "businesses_companies", :id => false, :force => true do |t|
+    t.integer "business_id"
+    t.integer "company_id"
+  end
 
   create_table "campaigns", :force => true do |t|
     t.integer  "business_id"

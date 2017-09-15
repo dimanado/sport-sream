7092b6bc2124020b0f70c9bae2967a41ecb24dae
dimanado <dima_nado@mail.ru>
2017-09-15 16:17:00 +0300

    render youtube video

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -135,4 +135,15 @@ module ApplicationHelper
     self.output_buffer = render(:file => "layouts/#{layout}")
   end
 
+  def youtube_video(youtube_url)
+    if youtube_url[/youtu\.be\/([^\?]*)/]
+      youtube_id = $1
+    else
+      youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+      youtube_id = $5
+    end
+    url = "http://www.youtube.com/embed/#{ youtube_id }"
+    render :partial => 'streams/video', :locals => { :url => url }
+  end
+
 end

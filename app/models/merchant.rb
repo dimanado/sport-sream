5edba581aaf9001efc626383ff6ceeb30ae111c2
gldstn <beetok1996@gmail.com>
2017-09-17 15:56:41 +0300

    Remove unused method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,8 +2,6 @@ class Merchant < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
-  after_create :update_dispatch_recepient#:send_welcome_email
-
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me,
     :businesses_attributes, :is_admin, :subscription_plan, :partner_id
@@ -21,14 +19,6 @@ class Merchant < ActiveRecord::Base
   validates_format_of :email, with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
   accepts_nested_attributes_for :businesses, reject_if: proc {|attrs| attrs.any?{|k,v| v.blank?} }
   validates_uniqueness_of :email
-
-
-  def update_dispatch_recepient
-    dispatch_recipient = DispatchRecipient.joins(:dispatch).order("created_at DESC").where("dispatches.partner_id" => self.partner_id, :email => self.email).readonly(false).first
-    unless dispatch_recipient.nil?
-      dispatch_recipient.update_attributes(status: "confirmed", updated_at: Time.now)
-    end
-  end
 
   def send_welcome_email
     HooddittMailer.welcome_merchant(email).deliver
